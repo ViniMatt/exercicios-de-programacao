22204bcaa3aa2823dd1d1db52a4fb4d0d8a08e67
ViniMatt <92060545+ViniMatt@users.noreply.github.com>
2022-01-21 15:56:08 -0300

    Program

---
+++ pedra_papel_tesoura/Jogo.cs
@@ -0,0 +1,43 @@
+﻿namespace pedra_papel_tesoura
+{
+    class Jogo
+    {
+        public int Jogador1 { get; set; }
+        public int Jogador2 { get; set; }
+
+        public Jogo (int jogador1, int jogador2)
+        {
+            Jogador1 = jogador1;
+            Jogador2 = jogador2;
+
+            if (jogador1 == 2 && jogador2 == 5)
+            {
+                System.Console.WriteLine("Jogador 1 venceu!");
+            }
+            else if(jogador1 == 5 && jogador2 == 2)
+            {
+                System.Console.WriteLine("Jogador 2 venceu!");
+            }
+            else if(jogador1 == 0 && jogador2 == 2)
+            {
+                System.Console.WriteLine("Jogador 1 venceu!");
+            }
+            else if (jogador1 == 2 && jogador2 == 0)
+            {
+                System.Console.WriteLine("Jogador 2 venceu!");
+            }
+            else if (jogador1 == 5 && jogador2 == 0)
+            {
+                System.Console.WriteLine("Jogador 1 venceu");
+            }
+            else if (jogador1 == 0 && jogador2 == 5)
+            {
+                System.Console.WriteLine("Jogador 2 venceu!");
+            }
+            else if(jogador1 == jogador2)
+            {
+                System.Console.WriteLine("Empate");
+            }
+        }
+    }
+}
---
+++ pedra_papel_tesoura/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace pedra_papel_tesoura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int jogador1, jogador2;
+
+            jogador1 = 0;
+            jogador2 = 0;
+
+            while (jogador1 != 9 && jogador2 != 9)
+            {
+                Console.WriteLine();
+                Console.WriteLine("2 = tesoura; 5 = papel; 0 = pedra;");
+                Console.WriteLine();
+
+                Console.Write("Jogador 1: ");
+                jogador1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.Write("Jogador 2: ");
+                jogador2 = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                new Jogo(jogador1, jogador2);
+            }
+            
+            
+
+                
+
+        }
+    }
+}
