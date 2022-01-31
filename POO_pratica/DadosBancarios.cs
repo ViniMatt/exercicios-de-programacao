a3320562391e725d44f3308d7de5a64af0a770d1
Matheus Vinicius <mattvinicius9811@gmail.com>
2022-01-31 18:15:16 -0300

    exercicio finalizado

---
+++ POO_pratica/DadosBancarios.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace POO_pratica
+{
+    class DadosBancarios
+    {
+        //objetos
+        public string Titular { get; set; }
+        public int Numero { get;private set; }
+        public double Saldo { get; private set; }
+
+        //construtores
+        public DadosBancarios(string nome, int numero)
+        {
+            Titular = nome;
+            Numero = numero;
+        }
+
+        public DadosBancarios(string nome, int numero, double saldo) : this(nome, numero)
+        { 
+           Saldo = saldo;
+        }
+
+        //metodos
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {Numero}, " +
+                $"Titular: {Titular}, " +
+                $"Saldo: ${Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+
+    }
+}
---
+++ POO_pratica/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_pratica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DadosBancarios p1;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá deposito inicial? ");
+            char dep =char.Parse(Console.ReadLine());
+
+            //--------------------------------------------
+            if (dep == 's' || dep == 'S')
+            {
+                Console.Write("Entre o valor para o deposito incial:");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                p1 = new DadosBancarios(titular, numero, depositoInicial);
+            }
+            else
+            {
+                p1 = new DadosBancarios(titular, numero);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(p1);
+            Console.WriteLine();
+
+            //-------------------------------------------------
+
+            Console.Write("Entre um valor para deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), (CultureInfo.InvariantCulture));
+            p1.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(p1);
+            Console.WriteLine();
+
+            //----------------------------------------------------
+
+            Console.Write("Entre um valor para saque:");
+            double saque = double.Parse(Console.ReadLine(), (CultureInfo.InvariantCulture));
+            p1.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(p1);
+        }
+    }
+}
